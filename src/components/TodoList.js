4cdbf2e904ce2b5132bbb45c09a29a57d470bec7
ernkeyz <exconzy26@gmail.com>
2023-09-08 21:58:22 +0000

    front-end completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,17 +2,6 @@ import React from 'react';
 
 const TodoList = ({todos, setTodos, setEditTodo}) => {
 
-    // const handleComplete = (todo) => {
-    //     setTodos(
-    //         todos.map((item) => {
-    //             if (item.id === todo.id) {
-    //                 return {...item, completed: !item.completed}
-    //             }
-    //             return item;
-    //         })
-    //     )
-    // }
-
     const handleEdit = ({ id }) => {
         const findTodo = todos.find((todo) => todo.id === id);
         setEditTodo(findTodo);
@@ -32,10 +21,6 @@ const TodoList = ({todos, setTodos, setEditTodo}) => {
                 onChange={(event) => event.preventDefault()}
             />
             <div className='control-btn'>
-                {/* <button className='button-complete task-button'
-                    onClick={() => handleComplete(todo)}>
-                    <i className='fa fa-check-circle'></i>
-                </button> */}
                 <button className='button-edit task-button'
                     onClick={() => handleEdit(todo)}>
                     <i className='fa fa-edit'></i>
