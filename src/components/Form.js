e99491ca712df7d6c89f232108d0be02caa96341
ernkeyz <exconzy26@gmail.com>
2023-09-09 20:16:16 +0000

    front-end completed update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { v4 as uuidv4 } from "uuid";
-import { addToDo } from '../utils/handelApi';
+// import { addToDo } from '../utils/handelApi';
 const Form = ({input, setInput, todos, setTodos, editTodo, setEditTodo}) => {
 
     const updateTodo = (title, id, completed) => {
@@ -45,7 +45,7 @@ const Form = ({input, setInput, todos, setTodos, editTodo, setEditTodo}) => {
         onChange={onInputChange}
         />
         <button className='button-add' type='submit'
-        onClick={addToDo}
+        // onClick={addToDo}
         >
             {editTodo ? "OK" : "Add"}
         </button>
