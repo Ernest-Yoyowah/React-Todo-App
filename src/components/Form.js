64969d4f7b3ffaebb6365e90160849283f21804d
ernkeyz <exconzy26@gmail.com>
2023-10-13 19:58:36 +0000

    updating components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,64 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { addToDo, updateEdit } from '../utils/handelApi';
+import { addToDo, updateEdit } from "../utils/handelApi";
 
+const Form = ({
+  setInput,
+  todos,
+  setTodos,
+  editTodo,
+  setEditTodo,
+  text,
+  setText,
+  todoId,
+  setTodoId,
+}) => {
+  const updateTodo = (text, _id, completed) => {
+    const newTodo = todos.map((todo) =>
+      todo._id === _id ? { text, _id, completed } : todo
+    );
+    setTodos(newTodo);
+    setEditTodo("");
+  };
 
-const Form = ({, setInput, todos, setTodos, editTodo, setEditTodo, text, setText, todoId ,setTodoId}) => {
+  useEffect(() => {
+    if (editTodo) {
+      setText(editTodo.text);
+    } else {
+      setText("");
+    }
+  }, [setText, editTodo]);
 
-    const updateTodo = (text, _id, completed) => {
-        const newTodo = todos.map((todo) =>
-            todo._id === _id ? {text, _id, completed} : todo
-        );
-        setTodos(newTodo);
-        setEditTodo("");
-    };
+  const onInputChange = (event) => {
+    setText(event.target.value);
+  };
 
-    useEffect(() => {
-        if (editTodo) {
-            setText(editTodo.text);
-        }
-        else {
-            setText("")
-        }
-    }, [setText, editTodo]);
-
-    const onInputChange = (event) => {
-        setText(event.target.value);
-    };
-
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        if (!editTodo) {
-            setTodos([...todos, {id: uuidv4(), text: text, completed: false }]);
-            setInput("");
-        }
-        else {
-            updateTodo(text, editTodo._id, editTodo.completed)
-        }
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (!editTodo) {
+      setTodos([...todos, { id: uuidv4(), text: text, completed: false }]);
+      setInput("");
+    } else {
+      updateTodo(text, editTodo._id, editTodo.completed);
+    }
+  };
 
   return (
     <form onSubmit={onFormSubmit}>
-        <input
-        type='text'
-        placeholder='Enter a Todo...'
-        className='task-input'
+      <input
+        type="text"
+        placeholder="Enter a Todo..."
+        className="task-input"
         value={text}
         required
         onChange={onInputChange}
-        />
-        <button
-        className='button-add'
-        type='submit'
+      />
+      <button
+        className="button-add"
+        type="submit"
         onClick={
-            editTodo
+          editTodo
             ? () => updateEdit(todoId, text, setTodos, setText, setEditTodo)
-            : () => addToDo(text, setText, setTodos)}
-        >
-            {editTodo ? "Save Edit" : "Add"}
-        </button>
+            : () => addToDo(text, setText, setTodos)
+        }
+      >
+        {editTodo ? "Save Edit" : "Add"}
+      </button>
     </form>
-  )
-}
+  );
+};
 
 export default Form;
